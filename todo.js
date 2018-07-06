ce2c21022d39525824ad8a6951fb1dfbf76d0afc
Fahad Ahmed <35034320+fahadahmed8@users.noreply.github.com>
2018-07-07 01:09:18 +0500

    Creating list and checkboxes

--- todo.js
+++ todo.js
@@ -1,4 +1,8 @@
 angular.module('TodoApp', [])
 	.controller('TodoCtrl', function($scope) {
 		$scope.totalTodos = 4;
+		$scope.todos = [
+			{text:'Learn angularJS', done:false},
+			{text:'Build an App', done:false},
+		];
 	});
